ff655ec68df1fa1d232808a8a3e24f902cda945b
HasanliHM <hacaga.hasanli@kapitalbank.az>
2024-02-04 20:41:49 +0400

    adding to toEqual() for deeply comparison instead of toBe()

--- src/output.test.js
+++ src/output.test.js
@@ -16,4 +16,28 @@ describe('generateResultText()', () => {
     expect(result2).toBeTypeOf('string')
     expect(result3).toBeTypeOf('string')
   });
+
+  it('should return a string that contains the calculation result if a number is provided as a result', () => {
+    const result = 5;
+
+    const resultText = generateResultText(result);
+
+    expect(resultText).toContain(result.toString());
+  });
+
+  it('should return an empty string if "no-calc" is provided as a result', () => {
+    const result = 'no-calc';
+
+    const resultText = generateResultText(result);
+
+    expect(resultText).toBe('');
+  });
+
+  it('should return a string that contains "Invalid" if "invalid" is provided as a result', () => {
+    const result = 'invalid';
+
+    const resultText = generateResultText(result);
+
+    expect(resultText).toContain('Invalid');
+  });
 });
--- src/util/numbers.test.js
+++ src/util/numbers.test.js
@@ -1,30 +1,51 @@
-import { it, expect } from 'vitest';
+import { it, expect, describe } from 'vitest';
 
-import { transformToNumber } from './numbers';
+import { cleanNumbers, transformToNumber } from './numbers';
 
-it('should transform a string number to a number of type number', () => {
-  const input = '1';
-
-  const result = transformToNumber(input);
-
-  expect(result).toBe(1).toBeTypeOf('number');
-});
-
-it('should transform a string number to a number of type number', () => {
+describe('transformToNumber()', () => {
+  it('should transform a string number to a number of type number', () => {
     const input = '1';
-  
+
     const result = transformToNumber(input);
-  
+
+    expect(result).toBe(1).toBeTypeOf('number');
+  });
+
+  it('should transform a string number to a number of type number', () => {
+    const input = '1';
+
+    const result = transformToNumber(input);
+
     expect(result).toBe(+input);
   });
 
-it('should yield NaN for non-transformable values', () => {
-  const input1 = 'invalid';
-  const input2 = {};
+  it('should yield NaN for non-transformable values', () => {
+    const input1 = 'invalid';
+    const input2 = {};
 
-  const result1 = transformToNumber(input1);
-  const result2 = transformToNumber(input2) 
+    const result1 = transformToNumber(input1);
+    const result2 = transformToNumber(input2);
 
-  expect(result1).toBeNaN();
-  expect(result2).toBeNaN();
+    expect(result1).toBeNaN();
+    expect(result2).toBeNaN();
+  });
+});
+
+describe('cleanNumbers()', () => {
+  it('should return an array of number values if an array of string number values is provided ', () => {
+    const numberValues = ['1', '2'];
+
+    const cleanedNumbers = cleanNumbers(numberValues);
+
+    // expect(cleanedNumbers[0]).toBeTypeOf('number');
+    expect(cleanedNumbers).toEqual([1, 2]) 
+  });
+
+  it('should throw an error if an array with at least one empty string is provided', () => {
+    const numberValues = ['', 1];
+
+    const cleanFn = () => cleanNumbers(numberValues);
+
+    expect(cleanFn).toThrow()
+  });
 });
